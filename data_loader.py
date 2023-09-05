ee1e22673e99445c5441a9a52b74ccbe56c441e3
SanathKumaraEng <eg183483@engug.ruh.ac.lk>
2023-09-05 23:59:22 +0530

    data_loader

--- data_loader.py
+++ data_loader.py
@@ -1,45 +1,30 @@
 from mpi4py import MPI
 import numpy as np
+import glob
 
-def load_data_subset(filename, start_idx, end_idx):
+def load_data_subset(filename):
     # Load data subset from the file
-    # Here, you can use any method to load data from the file
+    # Implement your data loading mechanism here
     # For example, using NumPy's loadtxt or load function
-    
-    # For illustration, generating dummy data
-    subset = np.arange(start_idx, end_idx)
-    return subset
+    data_subset = np.loadtxt(filename, delimiter=',')
+    return data_subset
 
-def distribute_data_to_processes(total_data_size, num_processes):
-    data_per_process = total_data_size // num_processes
-    remainder = total_data_size % num_processes
-    
-    data_ranges = []
-    start_idx = 0
-    for rank in range(num_processes):
-        end_idx = start_idx + data_per_process
-        if rank < remainder:
-            end_idx += 1
-        data_ranges.append((start_idx, end_idx))
-        start_idx = end_idx
-    
-    return data_ranges
-
-def distributed_data_loader(filename):
+def distributed_data_loader(filenames):
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
     size = comm.Get_size()
 
-    # Count the total number of data points in the file
-    with open(filename, 'r') as file:
-        total_data_size = sum(1 for line in file)
+    num_files = len(filenames)
+    files_per_process = num_files // size
+    remainder = num_files % size
+    start_file_idx = rank * files_per_process + min(rank, remainder)
+    end_file_idx = start_file_idx + files_per_process + (1 if rank < remainder else 0)
     
-    # Distribute data ranges to processes
-    data_ranges = distribute_data_to_processes(total_data_size, size)
-    
-    # Load data subset for this process
-    start_idx, end_idx = data_ranges[rank]
-    data_subset = load_data_subset(filename, start_idx, end_idx)
+    data_subset = []
+    for idx in range(start_file_idx, end_file_idx):
+        filename = filenames[idx]
+        subset = load_data_subset(filename)
+        data_subset.append(subset)
     
     # Gather data from all processes
     gathered_data = comm.allgather(data_subset)
@@ -47,11 +32,12 @@ def distributed_data_loader(filename):
     return gathered_data
 
 if __name__ == "__main__":
-    filename = "data.csv"
+    filenames = glob.glob("data_*.csv")  # List of your data files
     
-    gathered_data = distributed_data_loader(filename)
+    gathered_data = distributed_data_loader(filenames)
     
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
     
-    print(f"Process {rank} data:", gathered_data[rank])
+    for idx, data in enumerate(gathered_data[rank]):
+        print(f"Process {rank}, File {idx}, Data shape: {data.shape}")
