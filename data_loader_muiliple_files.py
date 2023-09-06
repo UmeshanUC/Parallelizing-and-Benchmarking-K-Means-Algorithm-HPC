a597fe6bac70f9d0dd8a6b3a0cae3cf3f7f12ba8
SanathKumaraEng <eg183483@engug.ruh.ac.lk>
2023-09-06 22:32:46 +0530

    data loaders created

--- data_loader_muiliple_files.py
+++ data_loader_muiliple_files.py
@@ -1,66 +1,67 @@
 from mpi4py import MPI
+import csv
 import numpy as np
 import glob
-import os
 
-def load_data_subset(filename):
-    # Load data subset from the file
-    # Implement your data loading mechanism here
-    # For example, using NumPy's loadtxt or load function
-    data_subset = np.loadtxt(filename, delimiter=',')
-    return data_subset
+def load_data_subset(filename, start_idx, end_idx, max_rows):
+    data_subset = []
+    with open(filename, 'r') as file:
+        reader = csv.reader(file, delimiter=',')
+        for idx, row in enumerate(reader):
+            if start_idx <= idx < end_idx:
+                # Convert row values to float as needed
+                data_row = [float(value) for value in row]
+                data_subset.append(data_row)
 
-
-def group_files(filenames, num_categories):
-    # Sort files by size in descending order
-    sorted_files = sorted(filenames, key=lambda x: os.path.getsize(x), reverse=True)
-
-    # Initialize categories with empty lists
-    categories = [[] for _ in range(num_categories)]
-
-    # Initialize total sizes for each category
-    category_sizes = [0] * num_categories
-
-    # Iterate through files and assign each to the category with the smallest total size
-    for file_path in sorted_files:
-        file_size = os.path.getsize(file_path)
-        smallest_category = min(range(num_categories), key=lambda i: category_sizes[i])
-        categories[smallest_category].append(file_path)
-        category_sizes[smallest_category] += file_size
-
-    return categories
+    # Pad with zeros if needed to reach max_rows
+    while len(data_subset) < max_rows:
+        data_subset.append([0.0] * len(data_subset[0]))
+    
+    return np.array(data_subset)
 
 def distributed_data_loader(filenames):
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
     size = comm.Get_size()
 
-    num_files = len(filenames)
+    gathered_data = []
 
-    # Sending files based on file size can be used to balance the load even better if needed
-    files_per_process = num_files // size
-    remainder = num_files % size
-    start_file_idx = rank * files_per_process + min(rank, remainder)
-    end_file_idx = start_file_idx + files_per_process + (1 if rank < remainder else 0)
-    
-    data_subset = []
-    for idx in range(start_file_idx, end_file_idx):
-        filename = filenames[idx]
-        subset = load_data_subset(filename)
-        data_subset.append(subset)
-    
-    # Gather data from all processes
-    gathered_data = comm.allgather(data_subset)
-    
+    for filename in filenames:
+        # Count the total number of data points in the file
+        with open(filename, 'r') as file:
+            total_data_size = sum(1 for _ in file)
+
+        # Distribute data ranges to processes
+        data_per_process = total_data_size // size
+        remainder = total_data_size % size
+        start_idx = rank * data_per_process
+        end_idx = start_idx + data_per_process
+
+        # Allocate remaining data points to some processes
+        if rank < remainder:
+            start_idx += rank
+            end_idx += rank + 1
+        else:
+            start_idx += remainder
+            end_idx += remainder
+
+        # Load data subset for this process
+        max_rows = total_data_size // size  # Determine max rows
+        data_subset = load_data_subset(filename, start_idx, end_idx, max_rows)
+
+        # Append data to the list
+        gathered_data.append(data_subset)
+
     return gathered_data
 
 if __name__ == "__main__":
     filenames = glob.glob("data_*.csv")  # List of your data files
-    
+
     gathered_data = distributed_data_loader(filenames)
-    
+
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
-    
-    for idx, data in enumerate(gathered_data[rank]):
+
+    # Here, gathered_data is a list of NumPy arrays with the same number of rows
+    for idx, data in enumerate(gathered_data):
         print(f"Process {rank}, File {idx}, Data shape: {data.shape}")
